7cd8d6a8b0bf6d5d450d111b05e2a07f190e8898
Jeypi04 <52103138+Jeypi04@users.noreply.github.com>
2019-07-03 21:22:24 +0800

    Delete menu.sh

--- menu.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#Menu
-
-echo -e "* menu      : Displays a list of available commands"
-echo -e "* create    : Creating an SSH Account"
-echo -e "* delete    : Clearing SSH and OpenVPN Account"
-echo -e "* exit      : exit Putty/Connecbot/"
-echo -e "              JuiceSSH"
-echo -e ""
